6bf3988b54c719551f1eb36e93642b639fd0dde1
skykanin <nicholas.jaunsen@gmail.com>
2016-11-08 14:04:27 +0100

    Mer på mal.js

--- Nettside/mal.js
+++ Nettside/mal.js
@@ -1,1 +1,7 @@
-var mq = window.matchMedia("(min-width: 1220px)");
+var mq = window.matchMedia("(max-width: 1220px)");
+
+if (mq.matches) {
+  // window width is less than 1220px
+} else {
+  // window width is at least 1220px
+}
