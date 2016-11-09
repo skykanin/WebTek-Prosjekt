00e625c0b01d1cff7c575c9be2c6bf432a5a9bbc
skykanin <nicholas.jaunsen@gmail.com>
2016-11-09 19:33:37 +0100

    La til linker i selector taggen på js.mal
    Fikset dette slik at links på selectoren fungerer. NB! Husk å lagre
    .html med riktig navn i forhold til listen jeg lagde på mal.js

--- Nettside/JS/mal.js
+++ Nettside/JS/mal.js
@@ -34,15 +34,22 @@ function Createdropdown(){
 	select.id = "select";
 	var name1 = ["option1", "option2", "option3", "option4", "option5", "option6", "option7"];
 	var name2 = ["a1", "a2", "a3", "a4", "a5", "a6", "a7"];
-	var valg = ["Om oss", "Hybel", "Tilbud", "Tips for studenter", "Kart", "Nyttig informasjon", "Kontakt oss"]
+	var valg = ["Om oss", "Hybel", "Tilbud", "Tips for studenter", "Kart", "Nyttig informasjon", "Kontakt oss"];
+	var verdi =["OmOss", "hybler", "tilbud", "tips", "map", "info", "kontakt"];
 
 	for (var i = 0; i < 7; i++) {
 		name1[i] = document.createElement('OPTION');
 		select.appendChild(name1[i]);
+		name1[i].value = verdi[i];
 		name2[i] = document.createElement('A');
 		name1[i].appendChild(name2[i]);
 		name2[i].innerHTML = valg[i];
 		name2[i].href = "/";
 	}
-
+	select.setAttribute("onchange", "slctV(this.value)");
 }
+
+function slctV(elm){
+	window.alert("value changed to: " + elm);
+	window.location= elm+".html";
+} 
