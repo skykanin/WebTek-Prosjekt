ed989e9641aa99d59c4c9408da38ad349e79fe89
skykanin <nicholas.jaunsen@gmail.com>
2016-11-10 12:22:37 +0100

    Fixed kode

--- Nettside/JS/mal.js
+++ Nettside/JS/mal.js
@@ -1,17 +1,3 @@
-var mq = window.matchMedia("(max-width: 1220px)");
-
-if (mq.matches) {
-  	// window width is less than 1220px
-	var nav = document.getElementsByTageName("nav")[0];
-	console.log(nav);
-	while (nav.firstChild) {
-    	nav.removeChild(myNode.firstChild);
-    }
-
-} else {
-  // window width is at least 1220px
-}
-=======
 // JavaScript Media Queries
 var mql = window.matchMedia("(max-width: 1000px)");
 mql.addListener(WidthChange);
@@ -63,4 +49,3 @@ function Createdropdown(){
 function slctV(site){
 	window.location= site+".html";
 } 
->>>>>>> origin/master
